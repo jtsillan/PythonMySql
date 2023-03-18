5e8e5f92ba8d1c3ec5fa567f9a16b732184509a2
jtsillan <juha.sillanpaa@edu.lapinamk.fi>
2023-03-18 14:52:40 +0200

    Update add_items.py add more data to be saved in database

--- items/add_items.py
+++ items/add_items.py
@@ -7,18 +7,39 @@ try:
     connection = mysql.connector.connect(user='root', host='127.0.0.1', database='suunnittelutehtava3')
     cursor = connection.cursor(dictionary=True, prepared=True)
 
+    check_category_query = ("SELECT * FROM categories GROUP BY id;")
+    cursor.execute(check_category_query)
+    categories = cursor.fetchall()
+
     name = input("Anna lisättävän itemin nimi: ")
     year = input("Anna vuosiluku: ")
     duration = input("Anna kesto: ")
     discription = input("Anna kuvaus: ")
     type = input("Anna tyyppi: (1)Elokuva, (2)Videopeli, (3)Äänikirja: ")
-    age_limit = input("Anna ikäraja: (1)10, (2)12, (3)14, (4)16, (5)18: ")
 
-    query = ("INSERT INTO items(name, year, duration, discription, item_types_id, age_limits_id) VALUES((%s), (%s), (%s), (%s), (%s), (%s))")
-    cursor.execute(query, (name, year, duration, discription, type, age_limit))
+    for category in categories:
+        print(f"({category['id']}) {category['category']}")
+
+    chosen_category = input("Valitse itemin kategoria yllä olevista vaihtoehdoista: (0), (1), jne ")
+
+    check_age_limits_query = ("SELECT * FROM age_limits;")
+    cursor.execute(check_age_limits_query)
+    age_limits = cursor.fetchall()
+
+    for age_limit in age_limits:
+        print(f"({age_limit['id']}) {age_limit['age_limit']}")
+
+    chosen_age_limit = input("Valitse itemin ikäraja yllä olevista vaihtoehdoista: (0), (1), jne ")
+
+    item_query = ("INSERT INTO items(name, year, duration, discription, item_types_id, age_limits_id) VALUES((%s), (%s), (%s), (%s), (%s), (%s));")
+    cursor.execute(item_query, (name, year, duration, discription, type, chosen_age_limit))
+    print("Lisätyn itemin id on: ", cursor.lastrowid)
+   
+    item_id = cursor.lastrowid
+    add_item_to_category_query = ("INSERT INTO items_has_categories(items_id, categories_id) VALUES((%s), (%s));")
+    cursor.execute(add_item_to_category_query, (item_id, chosen_category))
     connection.commit()
 
-    print("Lisätyn itemin id on: ", cursor.lastrowid)
 
 except mysql.connector.Error as err:
     print(err)

f5ea06afc36bcd598e5e79678a4a370ed4334923
jtsillan <juha.sillanpaa@edu.lapinamk.fi>
2023-03-18 15:14:57 +0200

    Update add_reviews_to_items.py, get reviews from table

--- items/add_reviews_to_items.py
+++ items/add_reviews_to_items.py
@@ -9,22 +9,38 @@ try:
 
     name = input("Anna itemin nimi: ")
 
+    # Make query to get all items to compare to input
     check_query = ("SELECT items.*, item_types.item_type, age_limits.age_limit FROM items INNER JOIN item_types "
              "ON items.item_types_id = item_types.id INNER JOIN age_limits ON items.age_limits_id = age_limits.id WHERE items.name = (%s);")
     cursor.execute(check_query, (name, ))
 
     items = cursor.fetchall()
 
+    # If item is not in database
     if items == []:
         print("Kohdetta ei löytynyt")
+
+    # Item is found from database
     else:
         for count, item in enumerate(items):
             print(f"({count}) Nimi: {item['name']}, Vuosi: {item['year']}, Tyyppi: {item['item_type']}, Kesto: {item['duration']} min, Ikäraja: {item['age_limit']}")
 
         choice = input("Valitse yllä olevista vaihtoehdoista: (0), (1), jne: ")
-        review = input("Anna itemille arvostelu: 1 / 1.5 / 2 / 2.5 / 3 / 3.5 / 4 / 4.5 / 5 : ")
+
+        # Make query to get all reviews from database
+        get_reviews_query = ("SELECT * FROM reviews;")
+        cursor.execute(get_reviews_query)
+
+        reviews = cursor.fetchall()
+        
+        for review in reviews:
+            print(f"({review['id']}) {review['review']}")
+
+        chosen_review = input("Valitse itemille arvostelu yllä olevista vaihtoehdoista: (1), (2), jne ")
         user_email = input("Anna käyttäjän sähköposti: ")
-        check_email_query = ("SELECT * FROM users WHERE email = (%s)")
+
+        # Make query and compare it to give email
+        check_email_query = ("SELECT * FROM users WHERE email = (%s);")
         cursor.execute(check_email_query, (user_email, ))
         user = cursor.fetchall()
 
@@ -34,8 +50,9 @@ try:
             item_id = items[int(choice)]['id']
             user_id = user[0]['id']
             
-            add_query = ("INSERT INTO reviews(review, users_id, items_id) VALUES((%s), (%s), (%s))")
-            cursor.execute(add_query, (review, user_id, item_id))
+            # Insert given values to 'items_has_reviews' table
+            add_query = ("INSERT INTO items_has_reviews(items_id, reviews_id, users_id) VALUES((%s), (%s), (%s));")
+            cursor.execute(add_query, (item_id, chosen_review, user_id, ))
             connection.commit()
 
             if cursor.rowcount == 1:
